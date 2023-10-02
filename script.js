338fb4c0e729308801f74754c5266832bd10880d
Shahu Patil <139709118+Shahu2002@users.noreply.github.com>
2023-10-02 13:33:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const timer = document.querySelector('#timer');
+const updateTimer = () => {
+  // Get the current date and time.
+  const now = new Date();
+
+  // Format the date and time.
+  const formattedDateTime = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
+
+  // Set the text of the `timer` element to the formatted date and time.
+  timer.textContent = formattedDateTime;
+};
+
+// Start the timer.
+setInterval(updateTimer, 1000);
